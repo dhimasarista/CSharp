e59a1add46c2a8db5e046b1c267618f92ea1a449
Dhimas Arista <dhimasharrisst@gmail.com>
2024-04-16 14:10:38 +0700

    Attach: Algorithms.FastSearch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Algorithms;
+
+int[] arr = {1,2,3,4,4,5,6,6,7,7,8,9,10};
+
+var search = new FastSearcher();
+int find = search.FindIndex(arr, 4);
+Console.WriteLine($"Ditemukan di index: {find}");
