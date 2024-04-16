a561187bba417d2f5e04b5881651b2524bbc9f19
Dhimas Arista <dhimasharrisst@gmail.com>
2024-04-16 14:54:28 +0700

    Attach: Networks.MySQL

---
+++ Networks/MySQL.cs
@@ -0,0 +1,41 @@
+using MySql.Data.MySqlClient;
+
+namespace Networks;
+
+class MySQL
+{
+    public MySQL(string dbName, string tableName){
+        string connectionString = $"server=localhost;user=root;database={dbName}";
+        
+        // Membuat koneksi ke database
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                // Buka koneksi
+                connection.Open();
+
+                // Query SQL
+                string query = $"SELECT * FROM {tableName}";
+
+                // Membuat perintah SQL
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    // Membaca data hasil query
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Menampilkan data
+                            Console.WriteLine(reader["name"].ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
-﻿using Algorithms;
+﻿using Networks;
 
-int[] arr = {1,2,3,4,4,5,6,6,7,7,8,9,10};
-
-var search = new FastSearcher();
-int find = search.FindIndex(arr, 4);
-Console.WriteLine($"Ditemukan di index: {find}");
+new MySQL("simfactory_dev", "departments");
