0cfa784fb65de333646a46de12b32fa43ffcd8c1
dhimasarista <dhimasharrisst@gmail.com>
2024-07-20 00:55:38 +0700

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,35 @@
-﻿class Program
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
 {
-    static void Main()
+
+    public static async Task Main(string[] args)
     {
-        string hello = "Hello, World!";
-        Console.WriteLine(hello);
+        // Concurrent execution of tasks using 'await'
+        var concurrentTasks = new List<Task>();
+        for (int i = 0; i < 4; i++)
+        {
+            concurrentTasks.Add(Task.Run(async () =>
+            {
+                Console.WriteLine($"Concurrent Task {i} started");
+                // Simulate some work
+                await Task.Delay(1000);
+                Console.WriteLine($"Concurrent Task {i} completed");
+            }));
+        }
+
+        // Wait for all concurrent tasks to complete
+        await Task.WhenAll(concurrentTasks);
+
+        // Parallel execution of tasks using TPL
+        var parallelTasks = new ParallelOptions { MaxDegreeOfParallelism = 4 };
+        Parallel.ForEach(Enumerable.Range(0, 4), parallelTasks, (i) =>
+        {
+            Console.WriteLine($"Parallel Task {i} started");
+            // Simulate some work
+            Thread.Sleep(1000);
+            Console.WriteLine($"Parallel Task {i} completed");
+        });
     }
 }
