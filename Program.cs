dcc7253fc27fa205b9992acae8f274c9016078b8
dhimasarista <dhimasharrisst@gmail.com>
2024-08-04 03:37:53 +0700

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿var numbers = new List<int>();
-var semaphore = new SemaphoreSlim(1);
-Parallel.For(0, 10, i =>
-{
-    semaphore.Wait();
-    numbers.Add(i);
-    semaphore.Release();
-});
-foreach (var x in numbers)
-{
-    Console.WriteLine(x);
-}
+﻿Console.WriteLine("Hello, C#");
