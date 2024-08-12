93a467bc21935bcf67a0d5e39c3b7a2e73231a12
dhimasarista <dhimasharrisst@gmail.com>
2024-08-13 00:53:21 +0700

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,36 @@
-using Networks;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var dhimas = new Programmer("Dhimas Arista");
+        dhimas.AddLanguage("C#");
+        dhimas.AddLanguage("Javascript");
+        dhimas.ShowLanguages();
+    }
+}
 
-var mysql = new MySQL("rekra_dev", "kecamatan");
+class Programmer
+{
+    private List<dynamic> _languages = [];
+    private string _name = "";
+    public Programmer(string name)
+    {
+        this._name = name;
+
+        Console.WriteLine($"Hello, {_name}");
+    }
+
+    public void AddLanguage(string language)
+    {
+        _languages.Add(language);
+    }
+
+    public void ShowLanguages()
+    {
+        Console.WriteLine("My Languages:");
+        foreach (var language in _languages)
+        {
+            Console.WriteLine($" - {language}");
+        }
+    }
+}
