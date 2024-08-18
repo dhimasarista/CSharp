9fc837b7feb4c15b17c60bc92a1d1ad5d1ee2541
dhimasarista <dhimasharrisst@gmail.com>
2024-08-19 00:50:10 +0700

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,9 +4,8 @@ class Program
 {
     static void Main()
     {
-        Point point = new();
-        point.x = 12;
-        point.y = 22;
+        Point point = new(12, 22);
+        
         int multiple = point.multiple();
         Console.WriteLine(multiple);
     }
@@ -17,6 +16,10 @@ public struct Point
     public int x;
     public int y;
 
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 
     public int multiple()
     {
