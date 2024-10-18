41f25deddfbeddf529c5ef602f0dbae089a31f3c
Dhimas Arista <dhimasharrisst@gmail.com>
2024-10-18 16:48:57 +0700

    update Program.cs and README.md

--- Program.cs
+++ Program.cs
@@ -1,34 +1,11 @@
 using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
 
 class Program
 {
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
-        const int iterations = 10_000_000;
-
-        // Task Parallel
-        var sw = Stopwatch.StartNew();
-        Parallel.For(0, iterations, i =>
-        {
-            var result = Math.Sqrt(i);
-        });
-        sw.Stop();
-        Console.WriteLine($"Task Parallel Execution Time: {sw.ElapsedMilliseconds} ms");
-
-        // Async
-        sw.Restart();
-        var tasks = new Task[iterations];
-        for (int i = 0; i < iterations; i++)
-        {
-            tasks[i] = Task.Run(() =>
-            {
-                var result = Math.Sqrt(i);
-            });
-        }
-        await Task.WhenAll(tasks);
-        sw.Stop();
-        Console.WriteLine($"Async Execution Time: {sw.ElapsedMilliseconds} ms");
+        int hex = 0xFF;
+        Console.WriteLine(hex == 255);
+        Console.WriteLine(hex == 0);
     }
 }
