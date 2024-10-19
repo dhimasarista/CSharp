4a1c927928bd9025014f6875056ec7720ae08474
Dhimas Arista <dhimasharrisst@gmail.com>
2024-10-19 19:10:05 +0700

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,2 @@
-int[] arr1 = {1, 2, 3};
-int[] arr2 = arr1; // Hanya alamat memori dari arr1 yang disalin ke arr2
-arr2[1] = 4;
-
-Console.WriteLine(arr1[1]); // output: 4
+object obj = 10;
+Console.WriteLine(typeof obj);
