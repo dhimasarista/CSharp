6434a87dd85d9a8cd3285a2fc2cef053014bdf3e
Dhimas Arista <dhimasharrisst@gmail.com>
2024-11-10 01:56:59 +0700

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main()
     {
-        Pilkada.Batam();
+        Foo foo = new("Dhimas Arista");
+        Bar bar = new(foo);
+        bar.DisplayName();
+    }
+}
+
+// Kelas Foo dengan konstruktor untuk menyuntikkan nama
+class Foo
+{
+    private string name;
+
+    public Foo(string name)
+    {
+        this.name = name;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
+
+// Kelas Bar dengan konstruktor untuk menyuntikkan objek Foo
+class Bar
+{
+    private Foo foo;
+
+    public Bar(Foo foo)
+    {
+        this.foo = foo;
+    }
+
+    public void DisplayName()
+    {
+        Console.WriteLine("Foo's name is: " + foo.GetName());
     }
 }
