e8d69a6d04fffc8ed9930329e9aa9ed715c65259
Dhimas Arista <dhimasharrisst@gmail.com>
2024-11-10 17:07:02 +0700

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,25 +6,8 @@ class Program
 {
     static void Main()
     {
-        // Set GC Latency Mode ke Batch
-        GCSettings.LatencyMode = GCLatencyMode.Batch;
-
-        Console.WriteLine("GC Latency Mode: Batch");
-
-        // Mengukur waktu eksekusi
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-
-        // Simulasi alokasi objek
-        for (int i = 0; i < 1000000; i++)
-        {
-            var obj = new object(); // Membuat objek baru
-        }
-
-        stopwatch.Stop();
-        Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
-
-        Console.WriteLine("Done!");
+        Car lascar = new();
+        lascar.Start();
     }
 }
 
