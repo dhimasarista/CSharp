6bb88e76b22b735089f23eff7ca957cc69f57fa7
dhimasarista <dhimasharrisst@gmail.com>
2025-01-19 16:08:59 +0700

    delete 3 files and update 1 file

--- Program.cs
+++ Program.cs
@@ -2,13 +2,23 @@ using System;
 
 class Program
 {
-    public static void Main()
+    static void Main()
     {
-        Console.WriteLine(typeof(NamaClass).Name);
+        // Alokasikan banyak objek
+        for (int i = 0; i < 100000; i++)
+        {
+            var obj = new object();
+        }
+
+        Console.WriteLine("Sebelum GC.Collect");
+        Console.WriteLine($"Memori yang digunakan: {GC.GetTotalMemory(false)} bytes");
+
+        // Memanggil GC secara manual
+        GC.Collect();
+        GC.WaitForPendingFinalizers(); // Tunggu semua finalizer selesai (opsional)
+        GC.Collect(); // Pastikan semua yang bisa dikoleksi sudah terkoleksi
+
+        Console.WriteLine("Setelah GC.Collect");
+        Console.WriteLine($"Memori yang digunakan: {GC.GetTotalMemory(false)} bytes");
     }
-}
-
-class NamaClass
-{
-
 }
