8e072e6769ca0c49f5d01b0c2c669b49d529a6e6
dhimasarista <dhimasharrisst@gmail.com>
2025-02-19 11:41:17 +0700

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-// Pengecekan tipe data
-object value = "Hello";
-if (value is string str)
+class Program
 {
-    Console.WriteLine("Value is a string: " + str);
+    public static void Main(string[] args)
+    {
+        var names = new List<string> { "dhimasarista", "anto" };
+        var hello = new Hello(names.ToArray());
+        string[] getData = hello.GetData();
+
+        foreach (var data in getData)
+        {
+            Console.WriteLine(data);
+        }
+    }
 }
-else
+
+struct Hello
 {
-    Console.WriteLine("Value is not a string");
+    private string[] data;
+    public Hello(string[] data)
+    {
+        this.data = data;
+    }
+
+    public readonly string[] GetData()
+    {
+        return this.data;
+    }
 }
